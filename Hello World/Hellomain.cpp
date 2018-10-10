20fd5651226806baca8f5c3d2fd9167b926e311c
Transcendentistry <eninakra@gmail.com>
2018-10-09 20:48:54 -0400

    Getting Hello World up and Running

---
+++ Hello World/Hellomain.cpp
@@ -0,0 +1,14 @@
+// Author: Joshua Thatcher
+// Purpose: Finishing the Hello World app and getting it on github correctly
+// Date: 10/9/2018
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+
+	cin.ignore();
+
+	return 0;
+}
